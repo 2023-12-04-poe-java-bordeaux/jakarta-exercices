33978241d4e6a7f39e80d99ceba0b4d9a5018efa
jcdominguez <jcd.pro@gmail.com>
2024-01-04 09:42:10 +0100

    démo attribut Servlet

--- src/main/java/com/demo/PrenomServlet.java
+++ src/main/java/com/demo/PrenomServlet.java
@@ -10,10 +10,22 @@ import java.io.IOException;
 @WebServlet("/salutation")
 public class PrenomServlet extends HttpServlet {
 
+    String prenom;
+    String nom;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
-            String lePrenom = request.getParameter("prenom");
-            response.getWriter().write("<h1>Bonjour, tu t'appelles:</h1>"+lePrenom);
+
+            String prenomQuery = request.getParameter("prenom");
+            if(prenomQuery != null)
+                prenom = prenomQuery;
+
+
+            String nomQuery = request.getParameter("nom");
+            if(nomQuery != null)
+                nom = nomQuery;
+
+            response.getWriter().write("<h1>Bonjour, tu t'appelles:</h1>"+prenom+"  "+nom);
     }
 }
