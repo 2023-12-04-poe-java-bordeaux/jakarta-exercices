bf9372ebfa3e06b4861d092d5d8fc2b478d95ad7
jcdominguez <jcd.pro@gmail.com>
2024-01-05 11:27:25 +0100

    TP todolist

---
+++ src/main/java/com/demo/TodolistServlet.java
@@ -0,0 +1,41 @@
+package com.demo;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/todolist")
+public class TodolistServlet extends HttpServlet {
+
+    ArrayList<String> todolist = new ArrayList<>();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        response.getWriter().write("<h1>Ajouter une tache</h1>");
+        response.getWriter().write("<form action='todolist' method='POST'>");
+        response.getWriter().write("<label>Nouvelle tache:</label>");
+        response.getWriter().write("<input type='text' name='tache'/>");
+        response.getWriter().write("<input type='submit' value='Envoyer'/>");
+        response.getWriter().write("</form>");
+
+        response.getWriter().write("<ul>");
+        for(String tache : todolist){
+            response.getWriter().write("<li>"+tache);
+        }
+        response.getWriter().write("</ul>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String nouvelleTache = request.getParameter("tache");
+        todolist.add(nouvelleTache);
+
+        response.getWriter().write("<a href='todolist'>Retour</a>");
+    }
+}
