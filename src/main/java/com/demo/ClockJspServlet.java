9e784b719de9ff7422d9ab2a20e836392eae6d07
jcdominguez <jcd.pro@gmail.com>
2024-01-05 14:21:35 +0100

    demo JSTL

--- src/main/java/com/demo/ClockJspServlet.java
+++ src/main/java/com/demo/ClockJspServlet.java
@@ -7,7 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 
 @WebServlet("/clockMvc")
@@ -17,8 +19,21 @@ public class ClockJspServlet extends HttpServlet {
             throws IOException, ServletException {
 
         LocalDateTime heure = LocalDateTime.now();
-
         request.setAttribute("heure", heure );
+
+        Boolean ilFaitJour = false;
+        request.setAttribute("ilFaitJour", ilFaitJour);
+
+        ArrayList<String> jourSemaine = new ArrayList<>();
+        jourSemaine.add("lundi");
+        jourSemaine.add("mardi");
+        jourSemaine.add("mercredi");
+        jourSemaine.add("jeudi");
+        jourSemaine.add("vendredi");
+        jourSemaine.add("samedi");
+        jourSemaine.add("dimanche");
+        request.setAttribute("jours", jourSemaine);
+
 
         request.getRequestDispatcher("WEB-INF/clock.jsp")
                 .forward(request, response);
--- src/main/webapp/WEB-INF/clock.jsp
+++ src/main/webapp/WEB-INF/clock.jsp
@@ -1,4 +1,5 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
   <head>
@@ -9,5 +10,24 @@
     <h1>Hello World! JSP</h1>
 
     <p>${ heure }</p>
+
+    <c:if test="${ ilFaitJour }">
+        <p>Bonjour !</p>
+    </c:if>
+
+   <c:if test="${ ! ilFaitJour }">
+    <p>Bonne nuit</p>
+    </c:if>
+
+    <p>Voici les jours de la semaine:</p>
+
+    <table>
+        <c:forEach var="jour" items="${jours}">
+            <tr>
+                <td> ${ jour } </td>
+            </tr>
+        </c:forEach>
+    </table>
+
   </body>
 </html>
